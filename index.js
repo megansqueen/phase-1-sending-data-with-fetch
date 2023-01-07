d021feb239240d82cf7f152444c053fdd93e8911
Megan <esler6724@gmail.com>
2023-01-06 19:51:08 -0800

    commit tree deb7d7b118b7823fb9486a6325305aba1d7ef9f9

--- index.js
+++ index.js
@@ -1,1 +1,104 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+
+// const { bodyParser } = require("json-server");
+
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarok!");
+//         console.log(error.message);
+//     })
+
+
+
+// function submitData(name = ``, email = ``){
+
+//     fetch("http://localhost:3000/users",{
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body:JSON.stringify(name, email)
+//     })
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (object) {
+//             console.log(object)
+//         })
+//     }
+
+
+// const userData = {
+//     name: ``,
+//     email: ``,
+// }
+
+// const configureData = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(userData),
+// };
+
+// function submitData() {
+//     fetch("http://localhost:3000/users", configureData)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert(message);
+//         console.log(error.message)
+//     })
+// }
+
+
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name: name, email: email})
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        let newId = object.id
+        document.createTextNode(newId)
+    })
+    .catch(function (error) {
+        alert("error")
+        console.log(error.message)
+    })
+}
+
+//append to the DOM
+//document.body.innerHTML
